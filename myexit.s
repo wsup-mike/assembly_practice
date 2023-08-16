6d91d57d2352e6bcd0f28881c11b6d9872b961ed
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-16 14:38:01 -0500

    Initial commit

---
+++ myexit.s
@@ -0,0 +1,9 @@
+# My first assembly program. This is a comment
+# Program purpose: Code uses system calls to exit the program. 
+# It sets up the exit syscall number 60 in the rax register and an exit status code 3 in the rdi register before finally invoking the syscall instruction
+.globl _start
+.section .text
+_start:
+    movq $60, %rax
+    movq $255, %rdi
+    syscall
