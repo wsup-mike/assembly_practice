64e968a268d4ebb6dc64d9b79ac963472958de77
Mike Ramos <michaelangeloram8@gmail.com>
2023-08-17 08:08:38 -0500

    Make mistake

--- myexit.s
+++ myexit.s
@@ -1,7 +1,7 @@
 # My first assembly program. This is a comment
 # Program purpose: Code uses system calls to exit the program. 
 # It sets up the exit syscall number 60 in the rax register and an exit status code 3 in the rdi register before finally invoking the syscall instruction
-.globl _start
+.globl _star
 .section .text
 _start:
     movq $60, %rax
